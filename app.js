ab096212d3d7ea61c4101d4326b81f33e6ed0313
kongukjae <51992661+kongukjae@users.noreply.github.com>
2023-11-09 21:28:04 +0900

    설계 작업 내용 꺼내보기 쉽게 정리하기

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// * project start!
+/**
+ * todo : web server 제작
+ * pseudo 1. express.js를 활용한다.
+ * pseudo 2. GET 요청에 대응한다.
+ * * 문서를 조립하는 route.js 구조
+ * * 예상되는 요청 "/"
+ * 
+ * pseudo 3. POST 요청에 대응한다.
+ * * action = "/mainPromptInputData"
+ * * method = "POST"
+ */
+
+
+/**
+ * todo : 찾아볼 기술
+ * pseudo 4. express.js
+ * * express.js의 기본적인 사용법
+ * * express static 기능
+ * * express router 기능
+ * * express middleware 기능
+ * * 찾아보면서 추가하기
+ * 
+ */
